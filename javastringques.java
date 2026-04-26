da9315763e713820ba15cf0f5875de7b9cbb01bd
vanshika344 <vanshikananwani252007@gmail.com>
2026-04-26 19:18:04 +0530

    add java string ques example progress

---
+++ javastringques.java
@@ -0,0 +1,18 @@
+public class javastringques {
+    public static void main(String[] args) {
+        
+    //1.//
+    String name = "vanshi is not a good person";
+    name = name.replace(" ", "_");
+
+System.out.println(name);
+//2//
+String letter  = "Dear <|name|>, Thanks a lot!";
+letter = letter.replace("<|name|>", "vanshi");
+System.out.println(letter);
+
+//3//
+String myString = "this string co"
+}
+}
+
